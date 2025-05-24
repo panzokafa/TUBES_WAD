c813e2109a20919226b0104c9837e0b7fc73a914
Rezkunz <Rezadwisaputra769@gmail.com>
2025-05-24 14:37:20 +0700

    xazxca

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,10 +5,4 @@ namespace App\Http\Controllers;
 abstract class Controller
 {
     //
-    kapkpakpkapkapkpakpakpakapakpkapkapkapkpakpakpak
-<<<<<<< HEAD
-    cirebon anjing
 }
-=======
-    jawa jawa jawa jawa
->>>>>>> a6c845a86139c04550f982695096ab5db6f3d756
